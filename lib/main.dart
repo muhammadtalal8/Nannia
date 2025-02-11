bbd2565fa373c658b18bbba29afc3550c6e9f351
muhammadtalal <muhammadtalalshakeel2009@gmail.com>
2025-02-12 01:17:35 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,13 @@ import 'package:nanniea/pages/firstpage.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
-
 class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
-  @override
+ @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
