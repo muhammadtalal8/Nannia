856c615f961a1a40d1315c973697090da89103e5
AA <muhammadtalalshakeel2009@gmail.com>
2025-02-18 23:25:40 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/pages/firstpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Nanniea',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const FirstPage(),
+    );
+  }
+}
