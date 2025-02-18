2be91766f3bc69d8d6f266b751280fc63d662249
AA <muhammadtalalshakeel2009@gmail.com>
2025-02-18 23:26:15 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ import 'package:flutter_practice/pages/firstpage.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
   State<MyApp> createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
- @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
